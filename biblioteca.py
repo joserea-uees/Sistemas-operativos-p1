9da0f29c6814e98c11442c71d2f482f526d9c2f3
JOSE MIGUEL REA CASTELO <jose.rea@uees.edu.ec>
2026-03-10 21:03:54 +0000

    [FEAT] - Metodo para agregar Libros y prestar libros

--- biblioteca.py
+++ biblioteca.py
@@ -25,3 +25,21 @@ class Biblioteca:
         self.inventario = {}           # dict[str, int]
         self.historial = []            # list[dict]
         self.lock = threading.Lock()   # Protección de secciones críticas
+
+    def agregarLibro(self, libro: Libro):
+        self.inventario[libro.titulo] = libro.copiasDisponibles
+
+    def prestarLibro(self, usuario: Usuario, titulo: str):
+        with self.lock:  # Sección crítica protegida
+            if titulo in self.inventario and self.inventario[titulo] > 0:
+                self.inventario[titulo] -= 1
+                usuario.librosPrestados.append(titulo)
+                self.historial.append({
+                    "usuario": usuario.nombre,
+                    "libro": titulo,
+                    "accion": "prestamo",
+                    "fecha": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                })
+                print(f"{usuario.nombre} prestó {titulo}. Copias restantes: {self.inventario[titulo]}")
+            else:
+                print(f"{usuario.nombre} no pudo prestar {titulo}: No disponible.")
