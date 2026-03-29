37083ced00cec3fb1ea43bd9ea4283396924b6e7
JOSE MIGUEL REA CASTELO <jose.rea@uees.edu.ec>
2026-03-29 22:04:29 +0000

    [FIX] - Actualizacion de los metodos de biblioteca y mas usuarios

--- main.py
+++ main.py
@@ -24,22 +24,22 @@ def main():
         Usuario("José Rea", 1),
         Usuario("Cristhian Guaman", 2),
         Usuario("Enma Castelo", 3),
-        Usuario("Allan Avendaño", 4)
+        Usuario("Allan Avendaño", 4),
+        Usuario("Max Rivas", 5),
+        Usuario("Diana Briones", 6),
+        Usuario("Danae Granizo", 7),
+        Usuario("Juan Pérez", 8)
+
     ]
 
     while True:
-        print("\n" + "="*50)
+        print("\n" + "/"*50)
         print("        SISTEMA BIBLIOTECA DIGITAL")
-        print("="*50)
+        print("/"*50)
         print("1. Ver estudiantes")
         print("2. Ver descripción del proyecto")
         print("3. Simulación automática")
-        print("4. Prestar libro")
-        print("5. Devolver libro")
-        print("6. Ver historial")
-        print("7. Ver inventario")
-        print("8. Crear usuario")
-        print("9. Limpiar bitácora")
+        print("4. Ver inventario")
         print("0. Salir")
 
         opcion = input("\nSeleccione una opción: ").strip()
@@ -53,46 +53,10 @@ def main():
         elif opcion == "3":
             simularConcurrencia(biblioteca, usuarios)
 
-        elif opcion in ["4", "5"]:
-            print("\nUsuarios disponibles:")
-            for i, u in enumerate(usuarios, 1):
-                print(f"{i}. {u.nombre}")
-
-            try:
-                idx = int(input("Seleccione usuario: ")) - 1
-                usuario = usuarios[idx]
-            except:
-                print("Usuario inválido")
-                continue
-
-            titulo = input("Título del libro: ").strip()
-
-            if opcion == "4":
-                usuario.solicitarPrestamo(biblioteca, titulo)
-            else:
-                usuario.devolverLibro(biblioteca, titulo)
-
-        elif opcion == "6":
-            biblioteca.mostrarHistorial()
-
-        elif opcion == "7":
+        elif opcion == "4":
             biblioteca.mostrarInventario()
 
-        elif opcion == "8":
-            nombre = input("Nombre del nuevo usuario: ").strip()
-            if nombre:
-                nuevo = Usuario(nombre, len(usuarios) + 1)
-                usuarios.append(nuevo)
-                print("Usuario creado correctamente")
-            else:
-                print("Nombre inválido")
-
-        elif opcion == "9":
-            confirmar = input("¿Seguro que deseas borrar la bitácora? (s/n): ").lower()
-            if confirmar == "s":
-                open("bitacora.log", "w").close()
-                print("Bitácora limpiada")
-
+     
         elif opcion == "0":
             print("Saliendo del sistema...")
             break
